ef4bfe26555859321c63dc8cde81a3c549ba54b7
nirmeetgandhi01 <nirmeetgandhi1@gmail.com>
2021-11-15 15:51:39 +0530

    change practice_set8_pr03.java

--- practice_set8_pr03.java
+++ practice_set8_pr03.java
@@ -1,4 +1,5 @@
 package com.company;
+import java.util.Scanner;
 //import com.sun.source.tree.BreakTree;
 
 class Square{
@@ -12,10 +13,13 @@ class Square{
 }
 public class practice_set8_pr03 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number to get Square of:");
+        int user = sc.nextInt();
 
         Square sq =  new Square();
-        System.out.println("The area of Square is  "+sq.getSide(3));
-        System.out.println("The parameter of square is "+sq.getPara(3));
+        System.out.println("The area of Square of "+ user +" is "+sq.getSide(user));
+        System.out.println("The parameter of square of "+ user+ " is "+sq.getPara(user));
 
     }
 }
